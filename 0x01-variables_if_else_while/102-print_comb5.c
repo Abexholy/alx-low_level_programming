919d84b0e38ce64cc0870302796ca623c911db08
Abexholy <abexholy123@gmail.com>
2023-03-20 02:04:39 -0700

    code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,13 +6,12 @@
 int main(void)
 {
 int n, m, l;
-for (n = 48; n < 58; n++)
 {
-for (m = 49; m < 58; m++)
+for (n = 48; n < 58; n++)
 {
 for (l = 50; l < 58; l++)
 {
-if (l > m && m > n)
+if (l > m & m > n)
 {
 putchar(n);
 putchar(m);
