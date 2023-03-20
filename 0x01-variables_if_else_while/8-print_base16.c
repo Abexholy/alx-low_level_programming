41e95264d7e1bf01e20204926f99d13e2373a57e
Abexholy <abexholy123@gmail.com>
2023-03-20 01:38:02 -0700

    code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,15 +5,14 @@
  */
 int main(void)
 {
-	int n;
+int n;
 for (n = 48; n < 58; n++)
 {
-	putchar(n);
-if (n != 57)
-{
-	putchar(',');
-	putchar(' ');
+putchar(n);
 }
+ for (n = 97; n < 103; n++)
+{
+putchar(',');
 }
 putchar('\n');
 return (0);
