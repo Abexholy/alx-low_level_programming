d82bcb5e29741a5b2c6be58a30725756241a6dc2
Abexholy <abexholy123@gmail.com>
2023-03-20 01:42:08 -0700

    code

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
  * main - prints all possible combinations of single-digit numbers
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-for (n = 48; n < 58; n++)
+int i;
+for (i = 48; i < 58; i++)
 {
-putchar(n);
+putchar(i);
 }
- for (n = 97; n < 103; n++)
+for (i = 97; i < 103; i++)
 {
-putchar(',');
+putchar(i);
 }
 putchar('\n');
 return (0);
