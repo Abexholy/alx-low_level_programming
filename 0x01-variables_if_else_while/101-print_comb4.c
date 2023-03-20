3bf7e06f16eccc110acee1da0cb5846edb0193b5
Abexholy <abexholy123@gmail.com>
2023-03-20 02:40:07 -0700

    code

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints all possible different combinations of three digits
- *Return: Always 0 (Success)                                                                                           */
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n, m, l;
