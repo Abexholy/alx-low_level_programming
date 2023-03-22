3ab433ace68e418156b2af7e621d27995a197c41
Abexholy <abexholy123@gmail.com>
2023-03-22 08:35:06 -0700

    code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,8 @@
  */
 void print_to_98(int n)
 {
-	int i, j;	
+	int i, j;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -15,7 +16,7 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d\n",i);
+				printf("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
