74091bf3a5555803be3fabcf9b7d632630db374d
Abexholy <abexholy123@gmail.com>
2023-05-03 00:45:09 -0700

    code

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list list : size_t print_listint(const listint_t *h);
+ *
+ * headerfile : head of list nodes
+ *
+ * Return the number of loops
+ *
+ */
+size_t print_listint(const listint_t *h);
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+
+	}
+	return (count);
+i}
