0c98e56d50f48fc27751f076985d4426e7c209e7
Abexholy <abexholy123@gmail.com>
2023-05-03 07:19:12 -0700

    code

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,15 +10,15 @@
 int pop_listint(listint_t **head)
 {
 	int mop;
-	listint_t *tank;
+	listint_t *temp;
 
 	if (*head == NULL)
 		return (0);
 
-	tank = *head;
+	temp = *head;
 	mop = (*head)->n;
 	*head = (*head)->next;
-	free(tank);
+	free(temp);
 
 	return (mop);
 }
