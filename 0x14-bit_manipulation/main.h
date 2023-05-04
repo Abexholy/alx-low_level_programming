7f75780faa75102c8fa715a00d524af1c26ce75c
Abexholy <abexholy123@gmail.com>
2023-05-04 08:00:06 -0700

    code

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,7 +7,7 @@
  * written in the directoty
  *
  */
-int _putchar;
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
