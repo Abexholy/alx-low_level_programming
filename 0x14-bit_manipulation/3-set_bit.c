bee1019c948298505d2aab08b34a1ccd12b4f163
Abexholy <abexholy123@gmail.com>
2023-05-04 08:16:24 -0700

    code

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,10 +5,10 @@
  * @n: The bit.
  * @index: The index to get the value at - indices start at 0.
  *
- * Return: If an error occurs - -1.
- *         Otherwise - The value of bit at index.
+ * Return: If an error occurs -1.
+ *         Otherwise 1
  */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
