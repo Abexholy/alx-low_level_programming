756e41f33d003f3c5bee94732d069641f7701970
Abexholy <abexholy123@gmail.com>
2023-06-13 21:28:51 -0700

    code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * create_array - code that creates an array of charts
- * @size: The tottal size of the array
- * @c : The chart to fill d array
- * Return - arraay
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
+ *
  */
 
 char *create_array(unsigned int size, char c)
