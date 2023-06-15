8e1ecda658c87842faa1b9eaf0b28b76947623f6
Abexholy <abexholy123@gmail.com>
2023-06-15 06:26:58 -0700

    code

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,12 +6,13 @@
  * Return: return void
  */
 
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
-void *s = malloc(b);
-if (s != null)
+	void *s = malloc(b);
+	if (s != NULL)
 {
 	return (s);
 }
 else
-exit 98;
+exit (98);
+}
