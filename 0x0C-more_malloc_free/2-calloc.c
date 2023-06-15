80e8f95efe7e9beef4205353c2fe2979f43e69eb
Abexholy <abexholy123@gmail.com>
2023-06-15 07:02:11 -0700

    code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include "main.h"
-/**
- * _calloc - Allocates the memory
+/*
+ * _calloc - allocates memory for an array
  * @nmeb: Number of the tottal members
  * @size: size of memory
  * Return - return values given
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int b =0, m =0;
-	char *a
+	int b = 0, m = 0;
+	char *a;
 
 		if (nmemb == 0 || size == 0)
 			return (NULL);
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	m = nmeb * size;
 	a = malloc(m);
 
-	if ( a == NULL);
+	if (a == NULL)
 	return (NULL);
 
 	while (b < a)
