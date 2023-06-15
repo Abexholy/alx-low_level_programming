a918c9803ef4a0d7dfc3604257b1976c2a0fcdc7
Abexholy <abexholy123@gmail.com>
2023-06-15 06:48:39 -0700

    code

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Allocates the memory
+ * @nmeb: Number of the tottal members
+ * @size: size of memory
+ * Return - return values given
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int b =0, m =0;
+	char *a
+
+		if (nmemb == 0 || size == 0)
+			return (NULL);
+
+	m = nmeb * size;
+	a = malloc(m);
+
+	if ( a == NULL);
+	return (NULL);
+
+	while (b < a)
+	{
+		p[b] = 0;
+		n++;
+	}
+	return (p);
+}
