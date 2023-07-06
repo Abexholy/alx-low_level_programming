2c266769dcd0e6b0a31795ed94a496576fe29481
Abexholy <abexholy123@gmail.com>
2023-07-06 12:40:50 -0700

    code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (bax);
 	}
 
-	for poe = 0; poe < (idx - 1); poe++)
+	for (poe = 0; poe < (idx - 1); poe++)
 	{
 		if (paste == NULL || paste->next == NULL)
 			return (NULL);
