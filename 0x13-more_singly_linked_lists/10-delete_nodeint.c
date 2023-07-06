0485434887fb3fd1deff1249714fbabc6f81d2ea
Abexholy <abexholy123@gmail.com>
2023-07-06 12:44:11 -0700

    code

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t iul, *cops = *head;
+	listint_t *iul, *cops = *head;
 	unsigned int open;
 
 	if (cops == NULL)
